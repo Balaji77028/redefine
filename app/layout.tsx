642e168713e14230d589d0a8a5b67c93cc021de2
revenger77025 <surajbalaji5680@gmail.com>
2024-07-30 19:57:50 +0530

    multiple changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-      appearance={{ variables: { colorPrimary: "#624cf5" } }}
-      afterSignOutUrl="/">
+    <ClerkProvider appearance={{ variables: { colorPrimary: "#624cf5" } }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
